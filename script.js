99af95ca780def1ed12f2b96c08cfc4c2922a7b5
Lauren <lburke522@gmail.com>
2022-08-17 22:23:59 -0400

    fixed typos

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="]
+var passLength = 8;
 
 // Write password to the #password input
 function askPrompts() {
@@ -15,15 +16,15 @@ function askPrompts() {
     return;
   }
   if (passLength<8 || passLength>128) {
-    alert("Must be between 8 and 128 characters");
+    alert("Must be between 8 and 128 characters)");
     writePassword();
   }
   else {
   alert("You must select at least one of the character types from the following 4 prompts \n(lowercase, uppercase, number, special character");
   }
 
-  var passLowercase = confirm("Click OK to include lowercase characters");
-  if (passLowercase === true) {
+  lowercase = confirm("Click OK to include lowercase characters");
+  if (lowercase === true) {
 console.log("confirmed");
 choiceArr = choiceArr.concat(lowerCase);
   } 
@@ -31,8 +32,8 @@ choiceArr = choiceArr.concat(lowerCase);
     console.log("declined");
   }
 
-  var passUppercase = confirm("Click OK to include uppercase characters");
-  if (passUppercase === true) {
+ uppercase = confirm("Click OK to include uppercase characters");
+  if (uppercase === true) {
     console.log("confirmed");
     choiceArr = choiceArr.concat(upperCase);
   } 
@@ -40,8 +41,8 @@ choiceArr = choiceArr.concat(lowerCase);
     console.log("declined");
   }
 
-  var passNumbers = confirm("Click OK to include numbers");
-  if (passNumbers === true) {
+  numbers = confirm("Click OK to include numbers");
+  if (numbers === true) {
     console.log("confirmed");
     choiceArr = choiceArr.concat(numbers);
   } 
@@ -49,14 +50,23 @@ choiceArr = choiceArr.concat(lowerCase);
     console.log("declined");
   }
 
-  var passSpecialChars = confirm("Click OK to include special characters");
-  if (passSpecialChars === true) {
+  specialChars = confirm("Click OK to include special characters");
+  if (specialChars === true) {
     console.log("confirmed");
     choiceArr = choiceArr.concat(specialChars);
   } 
-  else{
+  else{  
+    console.log("declined");
   }
 }  
+
+function generatePassword() {
+  var password = "";
+  for (var i = 0; i < passLength; i++) {
+    password = password + choiceArr(Math.floor(Math.random() * choiceArr.length));
+}
+return password;
+}
 
   
 
@@ -73,14 +83,6 @@ function writePassword () {
 
 }
 
-function generatePassword() {
-  var password = "";
-  for (var i = 0; i < passLength; i++) {
-    var index = Math.floor(Math.random() * choiceArr.length);
-    password = password + choiceArr[index];
-}
-return password;
-}
 
 
 
